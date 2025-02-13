41bf55b55af54b53d9771f9ffa4f4e82c5a3a219
webgliders <haleemcoco14@gmail.com>
2025-02-13 16:44:16 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -41,10 +41,16 @@ async def create_book(book: Book):
     )
 
 
+@router.get(
+    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
+)
+async def get_books() -> OrderedDict[int, Book]:
+    return db.get_books()
+
 
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int, book: Book) -> JSONResponse:
+async def get_book(book_id: int) -> JSONResponse:
     book = db.books.get(book_id)
     if not book:
         return JSONResponse(
